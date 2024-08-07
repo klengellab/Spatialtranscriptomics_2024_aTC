002b539efda9a6db61f2ef7519d1f1e25123ff54
klengellab <49945496+klengellab@users.noreply.github.com>
2024-08-07 09:54:42 -0400

    Update 3-1_WGCNA.R

--- network_analysis/3-1_WGCNA.R
+++ network_analysis/3-1_WGCNA.R
@@ -193,72 +193,11 @@ plotDendroAndColors(modules$dendrograms[[1]], colors = datColors,
 dev.off()
 
 ## Calculation eigengenes
-eigengene_network <- function(Expr, module, metadata){
-  moduleLabels <- module$colors
-  moduleColors = labels2colors(moduleLabels)
-  # plot eigen gene network
-  ME1 <- moduleEigengenes(Expr, moduleColors)$eigengenes
-  MET = orderMEs(cbind(ME1, metadata))
-  pdf(paste0(output_path, "eigengene_network_signed.pdf"))
-  plotEigengeneNetworks(MET, "", marDendro = c(0.5,2,0.5,2), marHeatmap = c(4,6,2,6),
-                        cex.lab = 0.4, xLabelsAngle=90)
-  dev.off()
-  
-  return(ME1)
-}
 ME1 <- eigengene_network(Expr = data, module = modules, metadata = traitData)
 
 ## Relate modules to phenotype data
-module_trait <- function(Expr, metadata){
-  # Visualization of modules with the phenotype data
-  nGenes = ncol(Expr)
-  nSamples = nrow(Expr)
-  MEs0 <- moduleEigengenes(Expr, moduleColors)$eigengenes
-  MEs0 <- orderMEs(MEs0)
-  modTraitCor = stats::cor(MEs0, metadata, use = "p", method = "pearson")
-  modTraitP = corPvalueStudent(modTraitCor, nSamples)
-  textMatrix = paste(signif(modTraitCor, 2), "\n(",
-                     signif(modTraitP, 1), ")", se = "")
-  dim(textMatrix) = dim(modTraitCor)
-  pdf(paste0(output_path, "module_trait_relationship_raw_signed.pdf"), width = 12, height = 10)
-  par(mar = c(4,8,2,2))
-  labeledHeatmap(Matrix = modTraitCor, xLabels = names(metadata), 
-                 yLabels = names(MEs0), ySymbols = names(MEs0),
-                 colorLabels = F, colors = blueWhiteRed(50),
-                 textMatrix = textMatrix, setStdMargins = F, cex.text = 0.3, cex.lab = 0.3,
-                 zlim = c(-1,1), main = paste("Module-trait relationships"))
-  dev.off()
-  
-  return(MEs0)
-}
-MEs0 <- module_trait(data, traitData) # I have now the correct data and want to look further into the following modules
-# darkgrey, lightgreen, white, royalblue, orange
+MEs0 <- module_trait(data, traitData, 87) 
 
-# Relate modules to phenotype data with FDR adjustment
-module_trait <- function(Expr, metadata){
-  # Visualization of modules with the phenotype data
-  nGenes = ncol(Expr)
-  nSamples = nrow(Expr)
-  MEs0 <- moduleEigengenes(Expr, moduleColors)$eigengenes
-  MEs0 <- orderMEs(MEs0)
-  modTraitCor = stats::cor(MEs0, metadata, use = "p", method = "pearson")
-  modTraitP = corPvalueStudent(modTraitCor, nSamples)
-  modTraitP <- p.adjust(modTraitP, method = "BH", n = 87)
-  textMatrix = paste(signif(modTraitCor, 2), "\n(",
-                     signif(modTraitP, 1), ")", se = "")
-  dim(textMatrix) = dim(modTraitCor)
-  pdf(paste0(output_path, "module_trait_relationship_raw_signed_corrected_p.pdf"), width = 12, height = 10)
-  par(mar = c(4,8,2,2))
-  labeledHeatmap(Matrix = modTraitCor, xLabels = names(metadata), 
-                 yLabels = names(MEs0), ySymbols = names(MEs0),
-                 colorLabels = F, colors = blueWhiteRed(50),
-                 textMatrix = textMatrix, setStdMargins = F, cex.text = 0.3, cex.lab = 0.3,
-                 zlim = c(-1,1), main = paste("Module-trait relationships"))
-  dev.off()
-  
-  return(MEs0)
-}
-MEs0 <- module_trait(data, traitData) 
 # Gene relationship to traint and important modules: Gene significance and module membership
 ## calculate module membership values (aka. module eigengene based connectivity kME)
 datKME = signedKME(data, MEs0, corOptions = "use = 'p', method = 'pearson'")
@@ -321,75 +260,7 @@ for(module in names(hubgenes)){
 
 # GO, Kegg, reactome and TF enrichment analysis
 # I adapted the normal enrichment analysis function to include the transcription factor enrichment analysis
-enrichment_analysis <- function(moduleLabels, moduleColors, Expr, Labels1, output, universe, modules_interest){
-  enrichment <- list()
-  # keytype: character to identify the gene id's such as ENSEMBL, external_gene_name etc
-  Labels1$moduleColors <- moduleColors
-  Labels1$moduleColors = moduleColors
-  Labels1$genes <- colnames(Expr)
-  select_genes <- function(x){
-    require(dplyr)
-    y <- dplyr::filter(Labels1, moduleColors == x)
-    genes <- y$genes
-    return(as.data.frame(genes))
-  }
-  
-  # Getting the genes per module
-  Colours <- as.list(unique(Labels1$moduleColors))
-  names(Colours) <- unique(Labels1$moduleColors)
-  list_modules <- lapply(Colours, select_genes)
-  for(i in modules_interest){
-    enriched <- list()
-    message("GO analysis")
-    GO_datasets <- msigdbr(species = "Mus musculus", category = "C5")
-    # Select for only the BP, CC and MF category in GO analysis
-    filtered_GO_datasets <- GO_datasets[GO_datasets$gs_subcat %in% c("GO:CC", "GO:BP", "GO:MF"), ]
-    
-    x <- Colours[[i]]
-    genes <- list_modules[[x]][["genes"]]
-    enriched_GO <- as.data.frame(enricher(genes, universe = colnames(Expr), pvalueCutoff = 0.1,
-                                          qvalueCutoff = 0.1,pAdjustMethod = "BH",TERM2GENE = filtered_GO_datasets[, c("gs_name", "gene_symbol")],
-                                          TERM2NAME = filtered_GO_datasets[, c("gs_name", "gs_exact_source")]
-    ))
-    enriched <- append(enriched, list(enriched_GO))
-    message("KEGG analysis")
-    KEGG_datasets <- msigdbr(species = "Mus musculus", category = "C2", subcategory = "CP:KEGG")
-    enriched_KEGG <- as.data.frame(enricher(genes, universe = colnames(Expr), pvalueCutoff = 0.1, 
-                                            qvalueCutoff = 0.1, pAdjustMethod = "BH", TERM2GENE =
-                                              KEGG_datasets[,c("gs_name", "gene_symbol")]))
-    enriched_KEGG$Description <- str_sub(enriched_KEGG$ID,6) 
-    enriched_KEGG$Description <- str_replace_all(enriched_KEGG$Description, "_", " ")
-    
-    enriched <- append(enriched, list(enriched_KEGG))
-    # Reactome analysis
-    message("Reactome analysis")
-    reactome_datasets <- msigdbr(species = "Mus musculus", category = "C2", subcategory = "CP:REACTOME")
-    enriched_reactome <- as.data.frame(enricher(genes, universe = colnames(Expr), pvalueCutoff = 0.1, 
-                                                qvalueCutoff = 0.1, pAdjustMethod = "BH", TERM2GENE =
-                                                  reactome_datasets[,c("gs_name", "gene_symbol")]))
-    enriched_reactome$Description <- str_sub(enriched_reactome$Description, 10)
-    enriched_reactome$ID <- str_sub(enriched_reactome$ID, start = 1, end = 8)
-    
-    enriched <- append(enriched, list(enriched_reactome))
-    
-    # Transcription factor enrichment analysis
-    message("TF analysis")
-    TF_datasets <- msigdbr(species = "Mus musculus", category = "C3")
-    x <- which(TF_datasets$gs_subcat %in% c("TFT:TFT_Legacy", "TFT:GTRD"))
-    TF_datasets <- TF_datasets[x,]
-    enriched_tf <- as.data.frame(enricher(genes, universe = colnames(Expr), pvalueCutoff = 0.05, 
-                                          qvalueCutoff = 0.1, pAdjustMethod = "BH", TERM2GENE =
-                                            TF_datasets[,c("gs_name", "gene_symbol")]))
-    enriched_tf$Description <- str_sub(enriched_tf$Description, 10)
-    enriched_tf$ID <- str_sub(enriched_tf$ID, start = 1, end = 8)
-    
-    enriched <- append(enriched, list(enriched_tf))
-    names(enriched) <- c("GO", "KEGG", "Reactome", "TF")
-    enrichment <- append(enrichment, list(enriched))
-  }
-  names(enrichment) <- as.character(modules_interest)
-  return(enrichment)
-}
+
 modules_interest <- c("darkgrey", "lightgreen", "white", "royalblue", "orange")
 Label = as.data.frame(moduleLabels)
 # Running the GSEA analysis
